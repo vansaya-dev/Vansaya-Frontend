50257d3865003f3294d03866a7f40515293d10aa
Faizan hashmi <mdfaizanhashmi3@gmail.com>
2024-08-16 00:44:13 +0530

    first setup with favicon done

--- src/app/api/_api-interceptor.js
+++ src/app/api/_api-interceptor.js
@@ -1,47 +1,46 @@
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify'; 
 import { config } from "./configURL";
-import axios from "axios";
-import { useRouter } from "next/navigation";
+
 
 // Add a request interceptor
 
 const axiosHttp = axios.create({
   baseURL: `${config.baseURL}`,
 });
+
 axiosHttp.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
     if (token) {
-      config.headers["Authorization"] = "Bearer " + token;
+      config.headers['Authorization'] = `Bearer ${token} `;
     }
-    // config.headers['Content-Type'] = 'application/json';
-    // document.body.classList.add("loading-indicator");
     return config;
   },
-  (error) => {
-    Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 axiosHttp.interceptors.response.use(
-  (response) => {
-    document.body.classList.remove("loading-indicator");
-    return response;
-  },
+  (response) => response,
   (error) => {
-    document.body.classList.remove("loading-indicator");
     const originalRequest = error.config;
-    if (
-      error &&
-      error.response &&
-      error.response.status === 401 &&
-      originalRequest.url === "http://127.0.0.1:3000/v1/auth/token"
-    ) {
-      //.push('/login')
-      return Promise.reject(error);
+    const router = useRouter();
+
+    if (error.response && error.response.status === 401 && originalRequest.url !== config.refreshTokenUrl) {
+      // Handle unauthorized error
+      toast.error('Unauthorized. Please login.');
+      router.push('/login');
+    } else if (error.response && error.response.status === 403) {
+      // Handle forbidden error
+      toast.error('Forbidden. You do not have permission.');
+    } else {
+      // Other errors
+      toast.error('An error occurred.');
     }
 
     return Promise.reject(error);
   }
 );
 
-export { axiosHttp };
+export default axiosHttp;
