eb4e889815841b7f9a83b7125a85029a58d3ab33
Faizan hashmi <mdfaizanhashmi3@gmail.com>
2024-08-16 21:22:39 +0530

    checking api integration

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
+
+"use client"
 import Image from "next/image";
+import axiosHttp from "./api/_api-interceptor";
 
 export default function Home() {
+
+  const handleClick = async () => {
+    let response = await axiosHttp.get('/hello');
+    console.log(response, 'nigga response')
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    Hello Faizan boss is the hero on develop branch.
+      Hello Faizan boss is the hero on develop branch.
+      <button
+        onClick={handleClick}
+      >Click me please yrr😞</button>
     </main>
   );
 }
