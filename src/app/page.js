2bcc3295dee1fe5bc1e9e04be9c017ef80c0900c
Faizan hashmi <mdfaizanhashmi3@gmail.com>
2024-08-19 12:31:15 +0530

    test ruke

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-white">
-      Work in progress
+      Work in progress...
 
     </main>
   );
